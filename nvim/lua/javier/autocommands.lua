e1776e08db8472e3742e4ee3269060b1e4212b90
Javier Abrego <javier.abrego@revolucionmusic.com>
2025-03-11 22:20:15 +0100

    Commit with changes to: lazy-lock.json,autocommands.lua

--- nvim/lua/javier/autocommands.lua
+++ nvim/lua/javier/autocommands.lua
@@ -16,3 +16,18 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     vim.api.nvim_set_hl(0, "QuickScopeSecondary", { fg = "#5fffff", underline= true})
   end,
 })
+
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = vim.api.nvim_create_augroup("ts_imports", { clear = true }),
+  pattern = { "*.tsx,*.ts" },
+  callback = function()
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        only = { "source.removeUnused.ts" },
+        diagnostics = {},
+      },
+    })
+  end,
+})
